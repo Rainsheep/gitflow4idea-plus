50ec2d7953ef8297a3f9eb0f51071715d66eedd2
Rainsheep <1045674971@qq.com>
2023-06-11 16:54:21 +0800

    fix dispose error

--- src/main/java/gitflow/GitflowVersionTester.java
+++ src/main/java/gitflow/GitflowVersionTester.java
@@ -1,7 +1,6 @@
 package gitflow;
 
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Disposer;
@@ -13,68 +12,74 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class GitflowVersionTester {
 
-	private static final Logger logger = Logger.getInstance(GitflowVersionTester.class);
+    private static final Logger logger = Logger.getInstance(GitflowVersionTester.class);
 
-	private static final Map<Project, GitflowVersionTester> testers = new ConcurrentHashMap<>();
+    private static final Map<Project, GitflowVersionTester> testers = new ConcurrentHashMap<>();
 
-	public static GitflowVersionTester forProject(@NotNull Project project) {
-		return testers.computeIfAbsent(
-			project,
-			p -> {
-				Disposer.register(p, () -> testers.remove(p));
-				return new GitflowVersionTester(ApplicationManager.getApplication().getService(Gitflow.class), p);
-			}
-		);
-	}
+    public static GitflowVersionTester forProject(@NotNull Project project) {
+        if (project.isDisposed()) {
+            return new GitflowVersionTester(ApplicationManager.getApplication().getService(Gitflow.class), project);
+        }
 
-	@NotNull private final Gitflow gitflow;
-	@NotNull private final Project project;
+        return testers.computeIfAbsent(
+                project,
+                p -> {
+                    Disposer.register(p, () -> testers.remove(p));
+                    return new GitflowVersionTester(ApplicationManager.getApplication().getService(Gitflow.class), p);
+                }
+        );
+    }
 
-	private String version = null;
+    @NotNull
+    private final Gitflow gitflow;
+    @NotNull
+    private final Project project;
 
-	private GitflowVersionTester(@NotNull Gitflow gitflow, @NotNull Project project) {
-		this.gitflow = gitflow;
-		this.project = project;
-	}
+    private String version = null;
 
-	/**
-	 * <p>Returns the installed {@code git-flow} version. The version
-	 * is loaded on the first call to this method and is determined
-	 * by looking at the output of a {@code git flow version} command.</p>
-	 * <p>If the command fails, {@code null} is returned.</p>
-	 *
-	 * @return the {@code git flow} version, or {@code null}
-	 */
-	@Nullable
-	public String getVersion() {
-		return version;
-	}
+    private GitflowVersionTester(@NotNull Gitflow gitflow, @NotNull Project project) {
+        this.gitflow = gitflow;
+        this.project = project;
+    }
 
-	/**
-	 * Returns true if the {@code git flow} version can be determined
-	 * and is any AVH version ({@code #contains("AVH")}) and
-	 * not the unmaintained NVIE version.
-	 *
-	 * @return true if we think the git flow version is an AVH version.
-	 */
-	public boolean isSupportedVersion() {
-		return version != null && version.contains("AVH");
-	}
+    /**
+     * <p>Returns the installed {@code git-flow} version. The version
+     * is loaded on the first call to this method and is determined
+     * by looking at the output of a {@code git flow version} command.</p>
+     * <p>If the command fails, {@code null} is returned.</p>
+     *
+     * @return the {@code git flow} version, or {@code null}
+     */
+    @Nullable
+    public String getVersion() {
+        return version;
+    }
 
-	public void init(){
-		String returnedVersion = null;
-		try {
-			returnedVersion = gitflow.version(project).getOutputOrThrow();
-			logger.info("git flow version: " + version);
-		} catch (Exception e) {
-			logger.error("Could not determine git flow version", e);
-		}
-		if (returnedVersion != null){
-			version = returnedVersion;
-		}
-	}
-	
-	public boolean hasVersionBeenTested(){
-		return version != null;
-	}
+    /**
+     * Returns true if the {@code git flow} version can be determined
+     * and is any AVH version ({@code #contains("AVH")}) and
+     * not the unmaintained NVIE version.
+     *
+     * @return true if we think the git flow version is an AVH version.
+     */
+    public boolean isSupportedVersion() {
+        return version != null && version.contains("AVH");
+    }
+
+    public void init() {
+        String returnedVersion = null;
+        try {
+            returnedVersion = gitflow.version(project).getOutputOrThrow();
+            logger.info("git flow version: " + version);
+        } catch (Exception e) {
+            logger.error("Could not determine git flow version", e);
+        }
+        if (returnedVersion != null) {
+            version = returnedVersion;
+        }
+    }
+
+    public boolean hasVersionBeenTested() {
+        return version != null;
+    }
 }
